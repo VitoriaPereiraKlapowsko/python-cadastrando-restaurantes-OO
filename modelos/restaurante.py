03c3716751ee77a27b4b0eced62a0c11c6e8e8ec
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-02-25 18:29:57 -0300

    Altenando os estados dos restaurantes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -10,17 +10,22 @@ class Restaurante: #Criação de classe
     def __str__(self): # Serve para aparacer o que está escrito como Praça | Gourmet e outros com o uso do str
         return f'{self._nome} | {self.categoria}'
         
-    def listar_restaurantes(): #Criando um método para listar os restaurantes, assim não preciso ficar dando um print toda hora para mostrar as informações
+    @classmethod#Metodo que referencia a classe    
+    def listar_restaurantes(cls): #Criando um método para listar os restaurantes, assim não preciso ficar dando um print toda hora para mostrar as informações
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes: #O uso de Cls é por convenção
             print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo.ljust(25)}')
             
     @property #Modifico a forma de como aquele atributo vai ser lido
     def ativo(self):
         return '☑' if self._ativo else '☐' #Usando o coolsymbol para colocar estes simbolos
     
+    def alternar_estado(self): #Metodo para os objetos
+        self._ativo = not self._ativo
+    
 #Instanciando a Classe
 restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
