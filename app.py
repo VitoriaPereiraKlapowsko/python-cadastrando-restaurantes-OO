2ec5b3ec8eea71275ef8879010db6769dd813ded
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-03-17 16:16:14 -0300

    Aplicando descontos, usando conceito de classe abstrata e polimorfismo

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Paozinho', 2.00, 'Melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
 restaurante_praca.adiconar_no_cardapio(bebida_suco)
 restaurante_praca.adiconar_no_cardapio(prato_paozinho)
 
