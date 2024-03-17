f7714ac98ecaa8785d7bb6ca5e91ff60d891b1c2
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-03-17 15:38:55 -0300

    Adicionando itens no cardápio, usando a refatoração, mostrando os itens do cardapio

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
 prato_paozinho = Prato('Paozinho', 2.00, 'Melhor pão da cidade')
+restaurante_praca.adiconar_no_cardapio(bebida_suco)
+restaurante_praca.adiconar_no_cardapio(prato_paozinho)
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,5 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante: #Criação de classe
     restaurantes = []
@@ -8,6 +9,7 @@ class Restaurante: #Criação de classe
         self._categoria = _categoria.upper() #O upper serve para colocar tudo em maiusculo
         self._ativo = False #Com o uso do _ eu estou querendo dizer que este atributo é privado
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self) #Criei um objeto e a coloquei dentro de uma lista
         
     def __str__(self): # Serve para aparacer o que está escrito como Praça | Gourmet e outros com o uso do str
@@ -39,3 +41,25 @@ class Restaurante: #Criação de classe
         quantidade_de_notas = len(self._avaliacao) #Pegando o tamanho
         media = round(soma_das_notas / quantidade_de_notas, 1) #Arredondando em 1 as casas decimais
         return media
+    
+    #def adicionar_bebida_no_cardapio(self, bebida): #Adicionando itens no cardapio de forma ruim
+   #     self._cardapio.append(bebida)
+        
+   # def adicionar_prato_no_cardapio(self, prato):
+    #    self._cardapio.append(prato)
+    
+     #Adicionando itens no cardapio de uma forma bem mais resumida com o isinstance
+    def adiconar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio): #verificando se ele á um derivado da classe, assim pode colocar este item na lista
+            self._cardapio.append(item)
+            
+    @property
+    def exibir_cardapio(self): 
+        print(f'Cardapio do Restaurante {self._nome}\n')
+        for i, item in enumerate(self._cardapio, start=1): #coloquei um iteravel (i) e o enumerate para contar os indices na hora de listar os itens começando do 1 porque se não por padrão ele sempre vai iniciar em 0, fica como se fosse uma listinha 
+            if hasattr(item, 'descricao'): #se tiver o atributo, ou seja se ele tiver uma descrição vai aparecer se não o programa não quebra
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: R$ {item._preco} | Descrição: {item.descricao}'
+                print(mensagem_prato)
+            else:
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: R$ {item._preco} | Tamanho: {item.tamanho}'
+                print(mensagem_bebida)
